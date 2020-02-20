df62dbcbf5c51f985e4ce68378520d7912173c4c
Kerem ÇELİK <celikkerem1@gmail.com>
2020-02-20 20:21:09 +0300

    yeter

--- app/User.php
+++ app/User.php
@@ -6,8 +6,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
-{
-	
+{	
     use Notifiable;
     /**
      * The attributes that are mass assignable.
