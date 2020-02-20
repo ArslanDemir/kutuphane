c871feaaa2071945d7aa7cee2d86385f3179db60
arslan demir <isosaslan@gmail.com>
2020-02-20 20:38:22 +0300

    sahip cık

--- app/User.php
+++ app/User.php
@@ -8,7 +8,7 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {	
     use Notifiable;
-    /**
+    /** lütfen kodlara sahip çıkalım
      * The attributes that are mass assignable.
      *
      * @var array
